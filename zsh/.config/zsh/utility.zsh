e257ef78cb6b60d41dad00d7cd5e4f0597766ded
Muhammad Usman <usmandevstuff@gmail.com>
2025-05-06 15:54:35 +0500

    changes[hypr waybar alacritty zsh]

--- zsh/.config/zsh/utility.zsh
+++ zsh/.config/zsh/utility.zsh
@@ -3,6 +3,15 @@
 ##
 
 
+function updatemirrors() {
+  export TMPFILE="$(mktemp)"; \
+    sudo true; \
+    rate-mirrors --save=$TMPFILE arch --max-delay=43200 \
+      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist
+}
+
+
 ###---------------------ARCHIVE EXTRACT--------------------------###
 function ex() {
   if [ -f $1 ]; then
